cc0402bd9dbc0e764f648bc7fdd1b7b732161418
Bjarke Bondo Andersen <andersen.bjarke@gmail.com>
2013-04-30 14:52:29 +0200

    Start statement simplification

--- transformer/Simplifier.hs
+++ transformer/Simplifier.hs
@@ -43,6 +43,9 @@ sequenceNameGen (n:ns) = do (as, b) <- n
                             (as', bs) <- sequenceNameGen ns
                             return (as ++ as', b : bs)
 
+mapNameGen :: (a -> NameGen ([b], c)) -> [a] -> NameGen ([b], [c])
+mapNameGen f = sequenceNameGen . map f
+
 mkVar :: Ident a -> Expr a
 mkVar i = Var i $ ident_annot i
 
@@ -79,7 +82,7 @@ simplExpr e@ByteStrings{}    = return ([], e)
 simplExpr e@Strings{}        = return ([], e)
 simplExpr e@UnicodeStrings{} = return ([], e)
 simplExpr e@Call{} = do (funStmts, funVar) <- simplVar $ call_fun e
-                        (argStmts, argVars) <- sequenceNameGen . map simplArgument $ call_args e
+                        (argStmts, argVars) <- mapNameGen simplArgument $ call_args e
                         return (funStmts ++ argStmts, e{call_fun = funVar, call_args = argVars})
 -- Subscript
 -- SlicedExpr
@@ -90,19 +93,78 @@ simplExpr e@BinaryOp{} = do (leftStmts, leftVar) <- simplVar $ left_op_arg e
 simplExpr e@UnaryOp{} = do (stmts, var) <- simplVar $ op_arg e
                            return (stmts, e{op_arg = var})
 -- Lambda
-simplExpr e@Tuple{} = do (stmts, vars) <- sequenceNameGen . map simplVar $ tuple_exprs e
+simplExpr e@Tuple{} = do (stmts, vars) <- mapNameGen simplVar $ tuple_exprs e
                          return (stmts, e{tuple_exprs = vars})
 -- Yield
 -- Generator
 -- ListComp
-simplExpr e@List{} = do (stmts, vars) <- sequenceNameGen . map simplVar $ list_exprs e
+simplExpr e@List{} = do (stmts, vars) <- mapNameGen simplVar $ list_exprs e
                         return (stmts, e{list_exprs = vars})
 -- Dictionary
 -- DictComp
-simplExpr e@Set{} = do (stmts, vars) <- sequenceNameGen . map simplVar $ set_exprs e
+simplExpr e@Set{} = do (stmts, vars) <- mapNameGen simplVar $ set_exprs e
                        return (stmts, e{set_exprs = vars})
 -- SetComp
 -- Starred
 simplExpr e@Paren{} = simplExpr $ paren_expr e
 -- StringConversion
 simplExpr e = error $ "Simplifier.simplExpr called on unsupported expression: " ++ render (pretty e)
+
+-- Return ([simple statement])
+simplStmt :: Statement a -> NameGen [Statement a]
+--simplStmt s@Import{}          = 
+--simplStmt s@FromImport{}      = 
+simplStmt s@While{}           = do (condStmts, condVar) <- simplVar $ while_cond s
+                                   bodySuite <- simplSuite $ while_body s
+                                   elseSuite <- simplSuite $ while_else s
+                                   return $ condStmts ++ [ s{ while_cond = condVar
+                                                            , while_body = bodySuite
+                                                            , while_else = elseSuite
+                                                            } ]
+--simplStmt s@For{}             = 
+simplStmt s@Fun{}             = do (paramStmts, params) <- mapNameGen simplParameter $ fun_args s
+                                   (annotStmts, annotExpr) <- simplExprMaybe $ fun_result_annotation s
+                                   bodySuite <- simplSuite $ fun_body s
+                                   return $ paramStmts ++ annotStmts ++ [ s{ fun_args = params
+                                                                           , fun_result_annotation = annotExpr
+                                                                           , fun_body = bodySuite
+                                                                           } ]
+--simplStmt s@Class{}           = 
+--simplStmt s@Conditional{}     = 
+--simplStmt s@Assign{}          = 
+--simplStmt s@AugmentedAssign{} = 
+--simplStmt s@Decorated{}       = 
+--simplStmt s@Return{}          = 
+--simplStmt s@Try{}             = 
+--simplStmt s@Raise{}           = 
+--simplStmt s@With{}            = 
+--simplStmt s@Pass{}            = 
+--simplStmt s@Break{}           = 
+--simplStmt s@Continue{}        = 
+--simplStmt s@Delete{}          = 
+--simplStmt s@StmtExpr{}        = 
+--simplStmt s@Global{}          = 
+--simplStmt s@NonLocal{}        = 
+--simplStmt s@Assert{}          = 
+--simplStmt s@Print{}           = 
+--simplStmt s@Exec{}            = 
+simplStmt s = error $ "Simplifier.simplStmt called on unsupported statement: " ++ render (pretty s)
+
+simplSuite :: Suite a -> NameGen (Suite a)
+simplSuite = simplStmts
+
+simplStmts :: [Statement a] -> NameGen [Statement a]
+simplStmts ss = concat `liftM` mapM simplStmt ss
+
+-- Return ([simple statement], simple parameter)
+simplParameter :: Parameter a -> NameGen ([Statement a], Parameter a)
+simplParameter p@Param{} = do (annotStmts, annotExpr) <- simplExprMaybe $ param_py_annotation p
+                              (defaultStmts, defaultExpr) <- simplExprMaybe $ param_default p
+                              return (annotStmts ++ defaultStmts, p{ param_py_annotation = annotExpr
+                                                                   , param_default = defaultExpr})
+simplParameter p = error $ "Simplifier.simplParameter called on unsupported parameter type: " ++ render (pretty p)
+
+simplExprMaybe :: Maybe (Expr a) -> NameGen ([Statement a], Maybe (Expr a))
+simplExprMaybe Nothing     = return ([], Nothing)
+simplExprMaybe (Just expr) = do (exprStmts, expr') <- simplExpr expr
+                                return (exprStmts, Just expr')

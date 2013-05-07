fc060271a4c39eff35c7e6cab7c24f7b5098c4c3
Bjarke Bondo Andersen <andersen.bjarke@gmail.com>
2013-05-07 21:49:32 +0200

    Work some on SymbolicAnnotation

--- transformer/SymbolicAnnotation.hs
+++ transformer/SymbolicAnnotation.hs
@@ -1,27 +1,39 @@
-module SymbolicAnnotation (symbStmt) where
+module SymbolicAnnotation (symbStmt, symbModule) where
 
 import Control.Monad (msum)
+import Data.List (transpose)
 
 import Language.Python.Common.AST
 import Language.Python.Common.Pretty (pretty, render)
 import Language.Python.Common.PrettyAST ()
+import Language.Python.Common.SrcLocation ( SrcSpan(SpanEmpty) )
 
-mkIdent :: String -> Ident a
-mkIdent = error "SymbolicAnnotation.mkIdent not implemented"
+mkIdent :: String -> IdentSpan
+mkIdent s = Ident { ident_string = s, ident_annot = SpanEmpty }
 
-mkVar :: Ident a -> Expr a
-mkVar i = Var i $ ident_annot i
+mkVar :: IdentSpan -> ExprSpan
+mkVar i = Var i SpanEmpty
 
-mkCall :: Ident a -> [Expr a] -> Statement a
-mkCall = error "SymbolicAnnotation.mkCall not implemented"
+mkString :: IdentSpan -> ExprSpan
+mkString i = Strings { strings_strings = ["'" ++ ident_string i ++ "'"], expr_annot = SpanEmpty }
 
-mkIntroScope :: [Ident a] -> Statement a
+mkArg :: ExprSpan -> ArgumentSpan
+mkArg e = ArgExpr { arg_expr = e, arg_annot = SpanEmpty }
+
+mkCall :: IdentSpan -> [ExprSpan] -> StatementSpan
+mkCall i es = StmtExpr { stmt_expr = expr, stmt_annot = SpanEmpty }
+  where expr = Call { call_fun = mkVar i, call_args = map mkArg es, expr_annot = SpanEmpty }
+
+mkIntroScope :: [IdentSpan] -> StatementSpan
 mkIntroScope = error "SymbolicAnnotation.mkIntroScope not implemented"
 
-endScope :: Statement a
+endScope :: StatementSpan
 endScope = mkCall (mkIdent "symbolic_return") []
 
-symbStmt :: Statement a -> [Statement a]
+symbModule :: ModuleSpan -> ModuleSpan
+symbModule (Module m) = Module $ concatMap symbStmt m
+
+symbStmt :: StatementSpan -> [StatementSpan]
 --symbStmt s@Import{}          = 
 --symbStmt s@FromImport{}      = 
 --symbStmt s@While{}           = 
@@ -57,28 +69,41 @@ symbStmt s@Continue{} = [s]
 --symbStmt s@Exec{}            = 
 symbStmt s = badStatement s
 
-litAssIdent :: Ident a
+litAssIdent :: IdentSpan
 litAssIdent = mkIdent "symbolic_assign_literal"
 
-getLiteralAssign :: Ident a -> Expr a -> Maybe (Statement a)
-getLiteralAssign i e@Int{}            = Just $ mkCall litAssIdent [mkVar i, e]
-getLiteralAssign i e@LongInt{}        = Just $ mkCall litAssIdent [mkVar i, e]
-getLiteralAssign i e@Float{}          = Just $ mkCall litAssIdent [mkVar i, e]
-getLiteralAssign i e@Imaginary{}      = Just $ mkCall litAssIdent [mkVar i, e]
-getLiteralAssign i e@Bool{}           = Just $ mkCall litAssIdent [mkVar i, e]
-getLiteralAssign i e@None{}           = Just $ mkCall litAssIdent [mkVar i, e]
-getLiteralAssign i e@ByteStrings{}    = Just $ mkCall litAssIdent [mkVar i, e]
-getLiteralAssign i e@Strings{}        = Just $ mkCall litAssIdent [mkVar i, e]
-getLiteralAssign i e@UnicodeStrings{} = Just $ mkCall litAssIdent [mkVar i, e]
+callAssIdent :: IdentSpan
+callAssIdent = mkIdent "symbolic_assign_call"
+
+getLiteralAssign :: IdentSpan -> ExprSpan -> Maybe (StatementSpan)
+getLiteralAssign i e@Int{}            = Just $ mkCall litAssIdent [mkString i, e]
+getLiteralAssign i e@LongInt{}        = Just $ mkCall litAssIdent [mkString i, e]
+getLiteralAssign i e@Float{}          = Just $ mkCall litAssIdent [mkString i, e]
+getLiteralAssign i e@Imaginary{}      = Just $ mkCall litAssIdent [mkString i, e]
+getLiteralAssign i e@Bool{}           = Just $ mkCall litAssIdent [mkString i, e]
+getLiteralAssign i e@None{}           = Just $ mkCall litAssIdent [mkString i, e]
+getLiteralAssign i e@ByteStrings{}    = Just $ mkCall litAssIdent [mkString i, e]
+getLiteralAssign i e@Strings{}        = Just $ mkCall litAssIdent [mkString i, e]
+getLiteralAssign i e@UnicodeStrings{} = Just $ mkCall litAssIdent [mkString i, e]
 getLiteralAssign _ _                  = Nothing
 
-getCallAssignment :: Ident a -> Expr a -> Maybe (Statement a)
-getCallAssignment = error "SymbolicAnnotation.getCallAssignment not implemented"
+getCallAssignment :: IdentSpan -> ExprSpan -> Maybe (StatementSpan)
+getCallAssignment i e@Call{} | Var{} <- name
+                             , Just strs <- getArguments asString $ call_args e
+                             , Just vars <- getArguments asVar $ call_args e =
+  Just . mkCall callAssIdent $ [mkString i, mkString (var_ident name)] ++ concat (transpose [strs, vars])
+  where getArguments as args = mapM as args
+        asString a@ArgExpr{} | v@Var{} <- arg_expr a = Just . mkString $ var_ident v
+        asString _ = Nothing
+        asVar a@ArgExpr{} | v@Var{} <- arg_expr a = Just . mkVar $ var_ident v
+        asVar _ = Nothing
+        name = call_fun e
+getCallAssignment _ _ = Nothing
 
-getOpAssignment :: Ident a -> Expr a -> Maybe (Statement a)
+getOpAssignment :: IdentSpan -> ExprSpan -> Maybe (StatementSpan)
 getOpAssignment = error "SymbolicAnnotation.getOpAssignment not implemented"
 
-getIdent :: Statement a -> Expr a -> Ident a
+getIdent :: StatementSpan -> ExprSpan -> IdentSpan
 getIdent _ e@Var{} = var_ident e
 getIdent s _       = badStatement s
 
--- transformer/Utils.hs
+++ transformer/Utils.hs
@@ -4,13 +4,15 @@ import Control.Monad (liftM)
 import Data.Set (Set)
 import qualified Data.Set as Set
 
+import Language.Python.Common.AST (ModuleSpan)
 import Language.Python.Common.ParseError (ParseError)
-import Language.Python.Common.Pretty (pretty, render)
+import Language.Python.Common.Pretty (pretty, render, Pretty)
 --import Language.Python.Common.PrettyAST ()
 import Language.Python.Version3.Parser (parseExpr, parseStmt, parseModule)
 
 import Names (namesExpr, namesStmts, namesModule)
 import Simplifier (simplExpr, simplStmts, simplModule, evalNameGen)
+import SymbolicAnnotation (symbStmt, symbModule)
 
 getNames :: (String -> String -> Either ParseError (a,b)) -> (a -> Set String) -> String -> Set String
 getNames parse names = either (const Set.empty) (names . fst) . flip parse "" . (++"\n")
@@ -21,8 +23,11 @@ parseNamesInExpr = getNames parseExpr namesExpr
 parseNamesInStmt :: String -> Set String
 parseNamesInStmt = getNames parseStmt namesStmts
 
-parseNamesInModule :: String -> IO (Set String)
-parseNamesInModule fname = getNames parseModule namesModule `liftM` readFile fname
+parseNamesInModule :: String -> Set String
+parseNamesInModule file = getNames parseModule namesModule file
+
+parseNamesInModuleIO :: String -> IO (Set String)
+parseNamesInModuleIO fname = parseNamesInModule `liftM` readFile fname
 
 simplifyExpr :: String -> IO ()
 simplifyExpr str =  putStrLn $ either (("Error: "++) . show) (prettify . simplify) parsed
@@ -47,10 +52,16 @@ simplifyStmt str = putStrLn $ either (("Error: "++) . show) (prettify . simplify
         prettify stmts = concatMap ((++"\n") . render . pretty) stmts
 -- > simplifyStmt "if a+4 > b:\n a = b * a + 42\nelif b < 2*a:\n b = 2*a + 2*b + c**4\nelse:\n b = a * (b - 7)"
 
-simplifyModule :: String -> IO ()
-simplifyModule fname = do file <- (++"\n") `liftM` readFile fname
-                          names <- parseNamesInModule fname
-                          putStrLn . either (("Error: "++) . show) (prettify . simplify names) $ parseModule file ""
-  where simplify names (ast, _) = evalNameGen names $ simplModule ast
-        prettify = render . pretty
--- > simplifyModule "../interpreter/tests/simple1.py"
+simplifyModule :: String -> Either String ModuleSpan
+simplifyModule file = case parseModule (file++"\n") "" of
+  Left e         -> Left $ show e
+  Right (ast, _) -> Right . evalNameGen (parseNamesInModule file) $ simplModule ast
+-- > let Right mod = simplifyModule "a=c(d,e,42)"
+-- > putPretty $ symbModule mod
+
+simplifyModuleIO :: String -> IO ()
+simplifyModuleIO fname = readFile fname >>= putStrLn . either ("Error: "++) (render . pretty) . simplifyModule
+-- > simplifyModuleIO "../interpreter/tests/simple1.py"
+
+putPretty :: Pretty a => a -> IO ()
+putPretty = putStrLn . render . pretty

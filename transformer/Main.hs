26fb8d8a2f2d2c4431394aab23ba928968e4993e
Bjarke Bondo Andersen <andersen.bjarke@gmail.com>
2013-05-16 20:43:57 +0200

    Add main function

---
+++ transformer/Main.hs
@@ -0,0 +1,20 @@
+module Main (main) where
+
+import Control.Monad (when)
+import System.Environment (getArgs)
+import System.IO.Error (ioError, userError)
+
+import Language.Python.Common.Pretty (pretty, render)
+
+import Utils (instrumentModule)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  when (length args /= 2) . ioError $ userError "Two arguments required: input file and output file."
+  let [infile, outfile] = args
+  contents <- readFile infile
+  case instrumentModule contents of
+    Left err  -> ioError . userError $ "Program transformation failed: " ++ err
+    Right mod -> writeFile outfile  . render . pretty $ mod
+-- > ghc Main.hs && ./Main tests/simple1.py tests/simple1.out.py && echo 'Success! Program transformed.'

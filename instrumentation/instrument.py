bd2fbb43e1eecccf8582e038d7e1bbc11c0a46d4
Bjarke Bondo Andersen <andersen.bjarke@gmail.com>
2013-05-16 23:16:20 +0200

    Make the output slightly prettier

--- instrumentation/instrument.py
+++ instrumentation/instrument.py
@@ -2,34 +2,36 @@ import imp
 import builtins
 
 def symbolic_scope(*vars):
-    print('symbolic_scope: ', vars)
+    print('symbolic_scope:', vars)
 builtins.symbolic_scope = symbolic_scope
 
-def symbolic_assert(*vars):
-    print('symbolic_assert: ', vars)
+def symbolic_assert(var):
+    print('symbolic_assert:', var)
 builtins.symbolic_assert = symbolic_assert
 
-def symbolic_refute(*vars):
-    print('symbolic_refute: ', vars)
+def symbolic_refute(var):
+    print('symbolic_refute:', var)
 builtins.symbolic_refute = symbolic_refute
 
-def symbolic_call(*vars):
-    print('symbolic_call: ', vars)
+def symbolic_call(fun, *vars):
+    print('symbolic_call:', fun, '<-', vars)
 builtins.symbolic_call = symbolic_call
 
-def symbolic_assign_literal(*vars):
-    print('symbolic_assign_literal: ', vars)
+def symbolic_assign_literal(target, literal):
+    print('symbolic_assign_literal:', target, '=', literal)
 builtins.symbolic_assign_literal = symbolic_assign_literal
 
-def symbolic_assign_binop(*vars):
-    print('symbolic_assign_binop: ', vars)
+def symbolic_assign_binop(target, op, left, right):
+    print('symbolic_assign_binop:', target, '=', left, op, right)
 builtins.symbolic_assign_binop = symbolic_assign_binop
 
 bar = imp.load_source('module.name', '../transformer/tests/simple3.out.py')
-x = 3
+x = 8
 try:
     bar.foo(x)
 except ZeroDivisionError:
     print('When x is', x, 'a division by zero error occurs.')
 except:
     print('When x is', x, 'an unknown error occurs.')
+else:
+    print('No error occurs when x is', x)

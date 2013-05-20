9aceb0e64b70cdae735225e8a34f53a3133ef692
Bjarke Bondo Andersen <andersen.bjarke@gmail.com>
2013-05-20 17:10:43 +0200

    Automate program transformation and handle errors better

--- instrumentation/instrument.py
+++ instrumentation/instrument.py
@@ -1,5 +1,9 @@
 import imp
 import builtins
+import os.path
+from subprocess import call
+from sys import exc_info
+from traceback import extract_tb
 
 def symbolic_scope(*vars):
     print('symbolic_scope:', vars)
@@ -25,13 +29,44 @@ def symbolic_assign_binop(target, op, left, right):
     print('symbolic_assign_binop:', target, '=', left, op, right)
 builtins.symbolic_assign_binop = symbolic_assign_binop
 
-bar = imp.load_source('module.name', '../transformer/tests/simple3.out.py')
-x = 8
-try:
-    bar.foo(x)
-except ZeroDivisionError:
-    print('When x is', x, 'a division by zero error occurs.')
-except:
-    print('When x is', x, 'an unknown error occurs.')
-else:
-    print('No error occurs when x is', x)
+
+def get_transformed_path(path):
+    realpath = os.path.realpath(path)
+    prefix, ext = os.path.splitext(realpath)
+    outpath = prefix + '.out' + ext
+
+    transform = True
+    try:
+        before = os.path.getmtime(realpath)
+        after = os.path.getmtime(outpath)
+        if before < after:
+            transform = False
+    except: pass
+	
+    if transform:
+        print('Transform %s.' % realpath)
+        if call(['../transformer/Main', realpath, outpath]):
+            print('Transformation failed.')
+            return None
+        print('Transformation successful.')
+    else:
+        print("Don't transform %s." % realpath)
+
+    return outpath
+
+def main():
+    path = get_transformed_path('../transformer/tests/simple3.py')
+    if path is None: return
+
+    bar = imp.load_source('module.name', path)
+    x = 3
+    try:
+        bar.foo(x)
+    except:
+        type, e, trace = exc_info()
+        trace = '\n'.join(['%s line %d in function %s:\n  %s' % info for info in extract_tb(trace)[1:]])
+        print('When x is %s a %s exception is raised: %s\nTraceback:\n %s' % (str(x), type.__name__, e, trace))
+    else:
+        print('No error occurs when x is %s.' % str(x))
+
+if __name__ == '__main__': main()
